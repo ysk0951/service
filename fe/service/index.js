ebef9542537b9f9932dfe1d789f40650a66cfe6b
yskim <yskimweb@gmail.com>
2023-04-10 22:33:46 +0900

    feat : RN Element -> Native Base description : RN Element had error About 'react-native cli link"

--- fe/service/index.js
+++ fe/service/index.js
@@ -5,9 +5,14 @@
 import App from './App';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import {NativeBaseProvider} from 'native-base';
 import * as React from 'react';
 export default function Main() {
-  return <App />;
+  return (
+    <NativeBaseProvider>
+      <App />
+    </NativeBaseProvider>
+  );
 }
 
 AppRegistry.registerComponent(appName, () => Main);
